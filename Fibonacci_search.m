7b469435550d47be41ef71f3eae8a3f32dcdf42b
Prakhar Sharma <70601302+praksharma@users.noreply.github.com>
2021-04-05 18:44:31 +0100

    Add files via upload

---
+++ Fibonacci_search.m
@@ -0,0 +1,56 @@
+clear all
+% Assumption-the function is unimodal
+
+% Fibonacci function
+clc
+
+N=5;
+fprintf('N= %d\n',N)
+a=0;
+fprintf('Xl= %g\n',a)
+b=20;
+fprintf('Xu= %g\n',b)
+
+I_initial=b-a;
+fprintf('I_initial= %g\n',I_initial)
+
+for i=1:N
+    fprintf('\nIteration: %g\n',i)
+	I=fibo(N-i)/fibo(N)*I_initial;
+    fprintf('I= %g\n',I)
+    fprintf('Xl= %g\n',a)
+    fprintf('Xu= %g\n',b)
+	a_new=b-I;
+    fprintf('Xa= %g\n',a_new)
+	b_new=a+I;
+    fprintf('Xb= %g\n',b_new)
+	fprintf('f(Xa)= %g\n',func(a_new))
+    fprintf('f(Xb)= %g\n',func(b_new))
+	
+    % Looping
+	if func(a_new)<func(b_new)
+		b=b_new;
+	else 
+		a=a_new;
+    end
+    
+    % Stopping criteria
+    if func(a_new)==func(b_new)
+        break;
+    end
+end
+
+function num=fibo(N)
+f=[1,2,3,5,8];
+
+    if N==0
+        num=1;
+    else
+        num=f(N);
+    end
+
+end
+% Function evaluation
+function num1=func(x)
+	num1=x^2-8*x+19;%x^2-10*x+26;
+end
---
+++ Golden_section_search.m
@@ -0,0 +1,57 @@
+clear all
+% Assumption-the function is unimodal
+
+% Golden section search
+clc
+
+N=10; % maximum iteration
+a=0;
+fprintf('Xl= %g\n',a)
+b=pi/2;
+fprintf('Xu= %g\n',b)
+K=1.61803398875;
+fprintf('Golden ratio= %g\n',K)
+I=b-a;
+fprintf('I_initial= %g\n',I)
+tol=0.4;
+fprintf('Tolerance= %g\n',tol)
+
+for i=1:N
+    fprintf('\nIteration: %g\n',i)
+	I=I/K;
+    fprintf('I= %g\n',I)
+    
+	a_new=b-I;
+    fprintf('Xa= %g\n',a_new)
+	b_new=a+I;
+    fprintf('Xb= %g\n',b_new)
+	fprintf('f(Xa)= %g\n',func(a_new))
+    fprintf('f(Xb)= %g\n',func(b_new))
+    % looping
+    % for minimization use (<), and for maximization use (>)
+	if func(a_new)>func(b_new)
+		b=b_new;
+        disp('f(Xa)>f(Xb)')
+        
+	else 
+		a=a_new;
+        disp('f(Xa)<f(Xb)')
+    end
+    fprintf('Xl= %g\n',a)
+    fprintf('Xu= %g\n',b)
+    fprintf('abs(a-b)= %g\n',abs(a-b))
+    
+    % Stopping criteria
+    if abs(a-b)<tol
+       
+        break ;
+      
+    end
+    
+end
+
+
+% Function evaluation
+function num1=func(x)
+	num1=4*sin(x).*(1+cos(x));%x^2-8*x+19;
+end
